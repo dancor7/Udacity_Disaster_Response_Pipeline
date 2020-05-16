5ca0681a805fa350e43c617ecc5b7a197fc5a5e9
dancor7 <62025791+dancor7@users.noreply.github.com>
2020-05-15 21:57:29 -0400

    Update the structure in the model file

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -3,13 +3,8 @@
 
 # # ML Pipeline Preparation
 # Follow the instructions below to help you create your ML pipeline.
-# ### 1. Import libraries and load data from database.
-# - Import Python libraries
-# - Load dataset from database with [`read_sql_table`](https://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_sql_table.html)
-# - Define feature and target variables X and Y
 
-# In[22]:
-
+### 1. Import libraries and load data from database.
 
 # import libraries
 import pandas as pd
@@ -22,6 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.tree import DecisionTreeClassifier
 import warnings
 warnings.simplefilter("ignore")
 
@@ -36,11 +32,15 @@ nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 
-# In[2]:
 
-
-# load data from database
+### 2. Write a function to load your data
 def load_data():
+    """load data from database
+    INPUT
+    OUTPUT
+    X - features dataset
+    y - Target variables dataset
+    """
     engine = create_engine('sqlite:///data/DisasterResponse.db')
     df = pd.read_sql_table('clean_dataset', con=engine).head(10000)
     X = df['message']
@@ -48,19 +48,15 @@ def load_data():
     return X, y
 
 
-# In[3]:
-
-
-engine = create_engine('sqlite:///data/DisasterResponse.db')
-pd.read_sql_table('clean_dataset', con=engine).shape
-
-
-# ### 2. Write a tokenization function to process your text data
-
-# In[4]:
-
+### 3. Write a tokenization function to process your text data
 
 def tokenize(text):
+    """ Clean data and transform the text into tokens
+    INPUT
+    text - the data to be cleaned
+    OUTPUT
+    clean_tokens - the data cleaned and tokenized
+    """
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
@@ -76,144 +72,12 @@ def tokenize(text):
     return clean_tokens
 
 
-# ### 3. Build a machine learning pipeline
-# This machine pipeline should take in the `message` column as input and output classification results on the other 36 categories in the dataset. You may find the [MultiOutputClassifier](http://scikit-learn.org/stable/modules/generated/sklearn.multioutput.MultiOutputClassifier.html) helpful for predicting multiple target variables.
-
-# In[5]:
-
-
-model1 = Pipeline([
-    ('vect', CountVectorizer(tokenizer=tokenize)),
-    ('tfidf', TfidfTransformer()),
-    ('clf', MultiOutputClassifier(RandomForestClassifier()))])
-
-
-# ### 4. Train pipeline
-# - Split data into train and test sets
-# - Train pipeline
-
-# In[6]:
-
-
-X, y = load_data()
-y= y.astype(int)
-
-# split data, train and predict
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-
-# In[7]:
-
-
-
-#model1.fit(X_train.values, y_train.values)
-#y_pred = model1.predict(X_test)
-
-
-# ### 5. Test your model
-# Report the f1 score, precision and recall for each output category of the dataset. You can do this by iterating through the columns and calling sklearn's `classification_report` on each.
-
-# In[8]:
-
-
-# Get results and add them to a dataframe.
-def get_results(y_test, y_pred):
-    results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
-    num = 0
-    for cat in y_test.columns:
-        precision, recall, f_score, support = precision_recall_fscore_support(y_test[cat], y_pred[:,num], average='weighted')
-        results.set_value(num+1, 'Category', cat)
-        results.set_value(num+1, 'f_score', f_score)
-        results.set_value(num+1, 'precision', precision)
-        results.set_value(num+1, 'recall', recall)
-        num += 1
-    print('Aggregated f_score:', results['f_score'].mean())
-    print('Aggregated precision:', results['precision'].mean())
-    print('Aggregated recall:', results['recall'].mean())
-    return results
-
-
-# In[9]:
-
-
-#results = get_results(y_test, y_pred)
-#results
-
-
-# ### 6. Improve your model
-# Use grid search to find better parameters. 
-
-# In[10]:
-
-
-#model1.get_params()
-
-
-# In[13]:
-
-
-#parameters = {'clf__estimator__max_depth': [10, 50, None],
-#              'clf__estimator__min_samples_leaf':[2, 5, 10]}
-
-#model2 = GridSearchCV(model1, parameters)
-
-
-# ### 7. Test your model
-# Show the accuracy, precision, and recall of the tuned model.  
-# 
-# Since this project focuses on code quality, process, and  pipelines, there is no minimum performance metric needed to pass. However, make sure to fine tune your models for accuracy, precision and recall to make your project stand out - especially for your portfolio!
-
-# In[14]:
-
-
-
-#model2.fit(X_train.as_matrix(), y_train.as_matrix())
-#y_pred = model2.predict(X_test)
-
-
-#results2 = get_results(y_test, y_pred)
-#results2
-
-
-# In[15]:
-
-
-#model2.best_estimator_.get_params()
-
-
-# ### 8. Try improving your model further. Here are a few ideas:
-# * try other machine learning algorithms
-# * add other features besides the TF-IDF
-
-# In[16]:
-
-
-
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-
-# In[17]:
-
-
-from sklearn.tree import DecisionTreeClassifier
+### 4. Build a machine learning pipeline
 
 def model_pipeline():
+ """
+We create a pipeline from where the model shall be run
+ """
     pipeline = Pipeline([
         ('features', FeatureUnion([
 
@@ -231,34 +95,98 @@ def model_pipeline():
     return pipeline
 
 
-# In[18]:
+
+### 5. Write a function to obtain the f1 score, precision and recall for each output category of the dataset.
+
+def get_results(y_test, y_pred):
+    """ Obtain the metrics to evaluate the model
+    INPUT
+    y_test - the real values of the prediction
+    y_pred - the prediction by the model
+    OUTPUT
+    results - an object with the f-score, precision and recall values
+    """
+    results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
+    num = 0
+    for cat in y_test.columns:
+        precision, recall, f_score, support = precision_recall_fscore_support(y_test[cat], y_pred[:,num], average='weighted')
+        results.set_value(num+1, 'Category', cat)
+        results.set_value(num+1, 'f_score', f_score)
+        results.set_value(num+1, 'precision', precision)
+        results.set_value(num+1, 'recall', recall)
+        num += 1
+    print('Aggregated f_score:', results['f_score'].mean())
+    print('Aggregated precision:', results['precision'].mean())
+    print('Aggregated recall:', results['recall'].mean())
+    return results
+
+results = get_results(y_test, y_pred)
+results
 
 
-model3 = model_pipeline()
-model3.fit(X_train, y_train)
-y_pred = model3.predict(X_test)
+### 8. Build a class for extracting the starting verb of a text.
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+ """
+ Starting Verb Extractor class
+    
+ This class extract the starting verb of a sentence,
+ creating a new feature for the ML classifier
+ """
+    
+    def starting_verb(self, text):
+    
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 
-# In[19]:
 
 
-results3 = get_results(y_test, y_pred)
-results3
+### 9.Create the main class
+
+def main():
+    """Load the data, run the model and save model"""
+    
+    print('Loading data...)
+    X, y = load_data()
+    y= y.astype(int)
+
+    # split data, train and predict
+    print('Splitting data...)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    
+    print('Building model...')
+    model = model_pipeline()
+          
+    print('Training model...')
+    model.fit(X_train, y_train)
+          
+    print('Evaluating model...')
+    y_pred = model.predict(X_test)
+    
+    print('Trained model saved!')
+    pickle.dump(model, open('models/classifier.pkl', 'wb'))
+
+    
+
+if __name__ == '__main__':
+    main()
 
 
-# ### 9. Export your model as a pickle file
-
-# In[23]:
 
 
-
-pickle.dump(model3, open('models/classifier.pkl', 'wb'))
-
-
-# ### 10. Use this notebook to complete `train.py`
-# Use the template file attached in the Resources folder to write a script that runs the steps above to create a database and export a model based on a new dataset specified by the user.
-
-# In[ ]:
 
 
 
